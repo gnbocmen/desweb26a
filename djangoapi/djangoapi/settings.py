1cdbca94bd4dd14c755cc408d11383b4c5f14a08
gnbocmen <bocanegra.gm@gmail.com>
2026-03-12 16:31:54 +0100

    hola2

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -81,7 +81,11 @@ INSTALLED_APPS = [
     'buildings',
     'flowers',
     'accidentes',
+<<<<<<< Updated upstream
     'risk'
+=======
+    'buildings2'
+>>>>>>> Stashed changes
 ]
 
 MIDDLEWARE = [
